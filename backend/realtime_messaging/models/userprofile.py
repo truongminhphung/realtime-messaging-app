f4f2eb92f8cba7f706657a0b675de5722e7ded8b
Truong Minh Phung <truongminhphungcs@gmail.com>
2025-08-16 16:11:55 +0700

    fix: make updated_at column nullable in UserProfile model

--- backend/realtime_messaging/models/userprofile.py
+++ backend/realtime_messaging/models/userprofile.py
@@ -87,4 +87,4 @@ class UserProfile(Base):
     created_at = Column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
